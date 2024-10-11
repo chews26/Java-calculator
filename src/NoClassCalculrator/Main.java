2b96e72d34c600c58b5f15c661d2aa048b4e21d8
shine26 <chews26@naver.com>
2024-10-11 11:10:12 +0900

    클래스 없는 사칙연산계산기 1일차 작성

--- src/NoClassCalculrator/Main.java
+++ src/NoClassCalculrator/Main.java
@@ -9,25 +9,32 @@ public class Main {
         System.out.println("===사칙연산 계산기===");
 
         // 첫번째 계산할 숫자 입력
-        System.out.println("첫번째 숫자를 입력하세요 : ");
+        System.out.print("첫번째 숫자를 입력하세요 : ");
         int firstNum = scanner.nextInt();
 
         // 두번째 계산할 숫자 입력
-        System.out.println("두번째 숫자를 입력하세요 : ");
+        System.out.print("두번째 숫자를 입력하세요 : ");
         int secondNum = scanner.nextInt();
 
         // 계산에 사용할 사칙연산 기호 입력
-        System.out.println("원하는 사칙연산 기호를 입력하세요(+, -, *, % : ");
+        System.out.print("원하는 사칙연산 기호를 입력하세요(+, -, *, /) : ");
         char operator = scanner.next().charAt(0);
 
-    if (operator == '+') {
-        System.out.println(firstNum + secondNum);
-    } else if (operator == '-') {
-        System.out.println(firstNum - secondNum);
-    } else if (operator == '*') {
-        System.out.println(firstNum * secondNum);
-    } else if (operator == '/') {}
-
+        if (firstNum >= 0 && secondNum >= 0) {
+            if (operator == '+') {
+                System.out.println(firstNum + secondNum);
+            } else if (operator == '-') {
+                System.out.println(firstNum - secondNum);
+            } else if (operator == '*') {
+                System.out.println(firstNum * secondNum);
+            } else if (operator == '/') {
+                System.out.println(firstNum / secondNum);
+            } else {
+                System.out.println("사칙연산 값을 잘못 입력하셨습니다.");
+            }
+        } else {
+            System.out.println("양의 정수를 입력하세요.");
+        }
 
 
 
