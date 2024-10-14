fa3a4d62435eff021b031261871b6f0527e05339
shine26 <chews26@naver.com>
2024-10-14 18:01:10 +0900

    수정 : Class를 사용하여 while문이 동작하는 계산기 생성

--- src/ClassCalculrator/App.java
+++ src/ClassCalculrator/App.java
@@ -13,17 +13,17 @@ public class App {
 
         while (true) {
             System.out.print("첫번째 숫자를 입력하세요 : ");
-            int firstNum1 = scanner.nextInt();
+            int firstNum = scanner.nextInt();
             // 두번째 계산할 숫자 입력
             System.out.print("두번째 숫자를 입력하세요 : ");
-            int secondNum1 = scanner.nextInt();
+            int secondNum = scanner.nextInt();
             // 계산에 사용할 사칙연산 기호 입력
             System.out.print("원하는 사칙연산 기호를 입력하세요(+, -, *, /) : ");
-            char operator1 = scanner.next().charAt(0);
+            char operator = scanner.next().charAt(0);
             scanner.nextLine();
 
             // 결과 값 반환
-            int result = calculator.cal(firstNum1, secondNum1, operator1);
+            int result = calculator.cal(firstNum, secondNum, operator);
 
             System.out.println("더 계산을 원하시면 아무키나 눌러주세요 (exit 입력 시 종료)");
             String answer = scanner.nextLine();
